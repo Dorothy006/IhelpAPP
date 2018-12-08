784d8b9553e2e05ed93d919f5feca2db2a4fb8b0
Dongxue Hao <mshaodongxue@gmail.com>
2018-12-08 18:39:38 -0500

    123

---
+++ app/src/main/java/com/example/tayor/servpro/Model/Category.java
@@ -0,0 +1,13 @@
+package com.example.tayor.servpro.Model;
+
+public class Category {
+    private String categoryName;
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+}
